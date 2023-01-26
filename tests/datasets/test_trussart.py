0b4d3408ab940e7b5379ce8ce0ea0317181b3dbb
adrianomartinelli <adrianom@student.ethz.ch>
2023-01-26 14:10:19 +0100

    add check for equivalence with legacy code

--- tests/datasets/test_trussart.py
+++ tests/datasets/test_trussart.py
@@ -4,3 +4,16 @@ from ChromFormer.datasets.trussart import Trussart
 # %%
 def test_load_trussart():
     ds = Trussart()
+    assert True
+
+def test_trussart_data_equivalence():
+    from ChromFormer.Data_Tools.Data_Calculation import import_trussart_data
+    trussart_hic_old, trussart_structures_old = import_trussart_data('/Users/adrianomartinelli/projects/ChromFormer/data-old')
+
+    trussart = Trussart()
+    trussart_hic, trussart_structures = trussart.data
+
+    assert (trussart_hic == trussart_hic_old).all()
+
+    for new, old in zip(trussart_structures, trussart_structures_old):
+        assert (new == old).all()
