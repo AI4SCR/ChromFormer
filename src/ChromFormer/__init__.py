3430873325a569e2fe46d6386662366925e2cfe4
adrianomartinelli <adrianom@student.ethz.ch>
2023-01-26 14:29:40 +0100

    switch to src structure

---
+++ src/ChromFormer/__init__.py
@@ -0,0 +1,8 @@
+from . import datasets
+from . import generator
+from . import io
+from . import metrics
+from . import models
+from . import plotting as pl
+from . import processing as pp
+from . import utils
